6c3ead605cce7e93d27e5e9000cc1ff42f2f1cba
Joshua Campbell <me@joshuadcampbell.com>
2024-05-06 22:04:21 -0700

    ndi-monitor: init
    need to update ffmpeg source to ffmpeg-ndi

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22784,6 +22784,8 @@ with pkgs;
 
   ndi = callPackage ../development/libraries/ndi { };
 
+  ndi-monitor = callPackage ../applications/video/ndi-monitor { };
+
   neardal = callPackage ../development/libraries/neardal { };
 
   neatvnc = callPackage ../development/libraries/neatvnc { };
